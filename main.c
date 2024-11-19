940efa0e9853bc7197340937cd1af2c1bdbd4222
Manan R. Kumar <mananman23@gmail.com>
2024-11-19 16:26:38 +0530

    minor changes

--- main.c
+++ main.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void) {
+    struct Student {
+        char name[50];
+        int age;
+    }s1,ws2;
+    int t;
+    s1.age = 0;
+    strcpy(s1.name, "sad");
+    ws2.age = 10;
+
     return 0;
 }
