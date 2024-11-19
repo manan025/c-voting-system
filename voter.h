92ae7f7f77d2e7fd28c96b3721fe78bdda81f395
Manan R. Kumar <mananman23@gmail.com>
2024-11-19 19:09:13 +0530

    Created functions and headers

---
+++ voter.h
@@ -0,0 +1,14 @@
+//
+// Created by Manan Raina Kumar on 19/11/24.
+//
+
+#ifndef VOTER_H
+#define VOTER_H
+
+int vote(char*, char*, char*);
+int login(char*, char*);
+int signup();
+int logout();
+
+
+#endif //VOTER_H
