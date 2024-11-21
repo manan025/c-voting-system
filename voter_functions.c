13c0accc879f5ea71118b4117081261211336faf
Manan R. Kumar <mananman23@gmail.com>
2024-11-21 19:07:15 +0530

    minor changes

--- voter_functions.c
+++ voter_functions.c
@@ -1,4 +1,6 @@
+#include <stdio.h>
 #include "voter.h"
+#include <stdlib.h>
 /*
 * Functions for voters:
 * - save voter data
@@ -9,50 +11,47 @@
  */
 
 
-struct Users{
-		char name[100];
-		char email[50];
-		char password[15];
-		char access[5];
-		char sex[10];
-		int age;
-	};
+struct Users {
+    char name[100];
+    char email[50];
+    char password[15];
+    char access[5];
+    char sex[10];
+    int age;
+};
 
-	struct Votes{
-		char email[50];
-		char access_code[10];
-		char vote[5];
-	};
+struct Votes {
+    char email[50];
+    char access_code[10];
+    char vote[5];
+};
 
 int init_data(struct Users *users, struct Votes *votes) {
-
 }
 
 
-int vote(char* userid, char* access_code, char *vote_code) {
-  /*
- * Function to vote
- * Required Parameters: userid, access_code, vote_code
- * Returns: 0 if success, 1 if access code wrong, 2 if vote_code is invalid
-   */
+int vote(char *userid, char *access_code, char *vote_code) {
+    /*
+   * Function to vote
+   * Required Parameters: userid, access_code, vote_code
+   * Returns: 0 if success, 1 if access code wrong, 2 if vote_code is invalid
+     */
 }
 
-int save_data() {
-// do we really need this function?
+int save_votes(struct Votes *votes) {
+    FILE *vote_file;
+
 }
 
 // --- Authentication STARTS
 
-int login(char* userid, char* pwd) {
-
+int login(char *userid, char *pwd) {
 }
 
-int signup(char* name, char* userid, char* pwd, char* sex, int* age) {
-
+int signup(char *name, char *userid, char *pwd, char *sex, int *age) {
 }
 
 int logout() {
-
 }
 
 // --- Authentication ENDS
