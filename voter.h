0cf9d033aedb41a854c3c4b33913cf079eb6b1f8
Manan R. Kumar <mananman23@gmail.com>
2024-11-24 11:38:09 +0530

    signup function and initdata function

--- voter.h
+++ voter.h
@@ -17,10 +17,17 @@ typedef struct Users {
 } user;
 
 typedef struct Votes {
+    char uid;
     char email[50];
     char access_code[10];
     char vote[5];
 } votes;
+
+typedef struct questions {
+    char uid;
+    char q;
+    char options[][];
+};
 
 int init_data(struct Users, struct Votes);
 int vote(char*, char*, char*);
--- voter_functions.c
+++ voter_functions.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "voter.h"
 #include <stdlib.h>
+#include <secure/_string.h>
 /*
 * Functions for voters:
 * - save voter data
@@ -18,7 +19,24 @@ int len_users(user *users) {
     return n;
 }
 
+int len_votes(votes *vote) {
+    int n = 0;
+    while (vote[n].uid>0) ++n;
+    return n;
+}
+
 int init_data(struct Users *users, struct Votes *votes) {
+    FILE *vote_file, *user_file;
+    if ((vote_file = fopen("votes.bin", "rb")) == NULL) {
+        return 1;
+    }
+    if ((user_file = fopen("users.bin", "rb")) == NULL) {
+        return 1;
+    }
+    // read the data from the file and store it in the struct users and votes
+    fread(&users, sizeof(struct Users), 100, user_file);
+    fread(&votes, sizeof(struct Votes), 100, vote_file);
+    return 0;
 }
 
 
@@ -30,9 +48,13 @@ int vote(char *userid, char *access_code, char *vote_code) {
      */
 }
 
-int save_votes(struct Votes *votes) {
+int save_votes(votes *votes) {
     FILE *vote_file;
-
+    if ((vote_file = fopen("temp.bin", "wb")) == NULL) {
+        return 1;
+    }
+    fwrite(&votes, sizeof(struct Votes), len_votes(votes), vote_file);
+    return 0;
 }
 
 // --- Authentication STARTS
@@ -41,7 +63,23 @@ int login(char *userid, char *pwd, struct Users *users) {
 
 }
 
-int signup(char *name, char *userid, char *pwd, char *sex, int *age, user *users) {
+int signup(char *name, char *userid, char *pwd, char *sex, int age, user *users) {
+    int l = len_users(users);
+    strcpy(users[l].name, name);
+    strcpy(users[l].email, userid);
+    strcpy(users[l].password, pwd);
+    strcpy(users[l].role, "U");
+    strcpy(users[l].sex, sex);
+    users[l].age = age;
+
+    FILE *user_file;
+    if ((user_file = fopen("users.bin", "wb")) == NULL) {
+        return 1;
+    }
+
+    fwrite(&users, sizeof(struct Users), len_users(users), user_file);
+
+    return 0;
 
 }
 
